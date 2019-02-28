af0230ffb2a5f19f6c4355822b322c9ed6a3e6ab
Jeremy Plack <stlouisweb@gmail.com>
2019-02-28 13:30:45 -0600

    creates a responsive, nestable grid, with 2 break points and adjustable gutter size.

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'flex-grid'
+import FlexGrid from 'flex-grid'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <FlexGrid container gutter={30} align="space-around" justify="space-around">
+          <FlexGrid container item columns={{small: 12, medium: 6, large: 4}} key="item1">
+            <FlexGrid item columns={{small: 6}} key="item1a">
+              <div style={{border: '1px solid'}}>Item 1</div>
+            </FlexGrid>
+            <FlexGrid item columns={{small: 6}} key="item1b">
+              <div style={{border: '1px solid'}}>Item 2</div>
+            </FlexGrid>
+          </FlexGrid>
+          <FlexGrid item columns={{small: 12, medium: 6, large: 4}} key="item2">
+            <div style={{border: '1px solid'}}>
+              Item 3
+            </div>
+          </FlexGrid>
+          <FlexGrid item columns={{small: 12, medium: 6, large: 4}} key="item3">
+            <div style={{border: '1px solid'}}>
+              Item 4
+            </div>
+          </FlexGrid>
+        </FlexGrid>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,19 +3,89 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+export default class FlexGrid extends Component {
   static propTypes = {
-    text: PropTypes.string
+    align: PropTypes.string,
+    justify: PropTypes.string,
+    container: PropTypes.bool,
+    item: PropTypes.bool,
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+    ]).isRequired,
+    columns: PropTypes.shape({
+      small: PropTypes.number,
+      medium: PropTypes.number,
+      large: PropTypes.number
+    }),
+    gutter: PropTypes.number
+  }
+
+  getClassesFromProps() {
+    const {
+      container,
+      item,
+      columns
+    } = this.props
+    let classNames = `${container ? styles.container : ''} ${item ? styles.item : ''}`
+    if (columns) {
+      if (columns.small) {
+        classNames += ' ' + styles[`small-${columns.small}`]
+      } else {
+        classNames += ' ' + styles['small-12']
+      }
+
+      if (columns.medium) {
+        classNames += ' ' + styles[`medium-${columns.medium}`]
+      }
+
+      if (columns.large) {
+        classNames += ' ' + styles[`large-${columns.large}`]
+      }
+    }
+
+    console.log(classNames)
+    return classNames
   }
 
   render() {
     const {
-      text
+      align,
+      justify,
+      container,
+      item,
+      children,
+      gutter = 0
     } = this.props
 
+    const classes = this.getClassesFromProps()
+
+    const flexItems = React.Children.map(children, child => React.cloneElement(child, { gutter }))
+
+    const style = {}
+
+    if (container && !item) {
+      style.margin = '-' + gutter / 2 + 'px'
+      style.width = `calc(100% + ${gutter}px)`
+      style.alignContent = align
+      style.justifyContent = justify
+    }
+
+    if (container && item) {
+      style.width = `calc(100% + ${gutter}px)`
+      style.alignContent = align
+      style.justifyContent = justify
+      style.margin = 0
+      style.padding = 0
+    }
+
+    if (item && !container) {
+      style.padding = gutter / 2 + 'px'
+    }
+
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={classes} style={style}>
+        {flexItems}
       </div>
     )
   }
